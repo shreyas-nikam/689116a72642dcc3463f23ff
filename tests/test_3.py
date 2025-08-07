c8b0185e7c9d44061c4717df2e8209db2195f8f9
Shreyas Nikam <158344330+shreyas-nikam@users.noreply.github.com>
2025-08-07 10:25:26 -0400

    Add/update test_3.py file

--- tests/test_3.py
+++ tests/test_3.py
@@ -1,15 +1,17 @@
 import pytest
-from definition_7cf2e47b4d9c4e8a8201221bdb4b7ed3 import deterministic_pv
+from definition_9c8a1e18558e46e0a308112936f5bd1d import deterministic_pv
 import pandas as pd
 import numpy as np
 
-@pytest.mark.parametrize("cashflows, discount_rate, expected", [
-    (pd.Series([100, 100, 100]), 0.05, 272.324798),
-    (pd.Series([500, 400, 300]), 0.1, 994.74513),
-    (pd.Series([1000]), 0.0, 1000.0),
-    (pd.Series([]), 0.05, 0.0),
-    (pd.Series([-100, -50, 0, 50, 100]), 0.02, -4.0199)
+def create_series(data):
+    return pd.Series(data)
+
+@pytest.mark.parametrize("cashflows, expected", [
+    (create_series([100, 100, 100]), 300.0),
+    (create_series([100, -50, 25]), 75.0),
+    (create_series([0, 0, 0]), 0.0),
+    (create_series([1000]), 1000.0),
+    (create_series([-100, -100, -100]), -300.0)
 ])
-def test_deterministic_pv(cashflows, discount_rate, expected):
-    pv = deterministic_pv(cashflows, discount_rate)
-    assert np.isclose(pv, expected, rtol=1e-5)
+def test_deterministic_pv(cashflows, expected):
+    assert np.isclose(deterministic_pv(cashflows), expected)
